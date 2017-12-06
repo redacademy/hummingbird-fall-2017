6ca02899b5dceb6a63b6fd83a686e670ae705613
NCMoseley <ncmoseley@gmail.com>
2017-12-05 16:32:21 -0800

    Add footer info

--- themes/hummingbird/footer.php
+++ themes/hummingbird/footer.php
@@ -11,7 +11,21 @@
 
 			<footer id="colophon" class="site-footer page-footer" role="contentinfo">
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://github.com/redacademy/hummingbird-fall-2017' ); ?>"><?php printf( esc_html( 'Proudly developed by %s' ), 'Nate, Travis, Hamdy' ); ?></a>
+
+                <p>
+				<img src="/Assets/Elements/email icon.svg" alt="email icon"><span>info@hummingbirdnetwork.ca</span></p>
+                <p>
+					<img src="/Assets/Elements/phone icon.svg" alt="phone icon"><span>(250) 877.7082</span>
+				</p>
+                <p>
+					<img src="/Assets/Elements/facebook icon.svg" alt="facebook icon">
+                    <img src="/Assets/Elements/instagram icon.svg" alt="instagram icon">
+					<img src="/Assets/Elements/twitter icon.svg" alt="twitter icon">
+                </p>
+
+
+				<a href="<?php echo esc_url( 'https://github.com/redacademy/hummingbird-fall-2017' ); ?>"><?php printf( esc_html( 'Proudly developed by %s' ), 'Nate, Travis, Hamdy' ); ?></a>
+
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
