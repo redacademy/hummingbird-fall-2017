7e58b806fa7baf6206b5c80b7767a3fb17d951cb
Late Worm <travis.rempel@gmail.com>
2017-12-10 19:41:30 -0800

    Refactor footer for improved simplicity and semantics

--- themes/hummingbird/footer.php
+++ themes/hummingbird/footer.php
@@ -10,69 +10,75 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer page-footer" role="contentinfo">
+
+        <div class="footer-form">
+					<div class="width-content">
+					<?php get_sidebar(); ?>
+					</div>
+				</div>
+
+
 				<div class="site-info">
-				 
-          <div class="footer-form">
-						<?php get_sidebar(); ?>
-				  </div>
 
 					<div class="logo-white">
 						<img class="logo-small" src="<?php echo get_template_directory_uri(); ?>/assets/logos/large/symbol-white-large.png" alt="The Hummingbird Network logo">
 					</div>
 				
-					<div class="contact-details">
+					<div class="contact-details-container width-content">
+
+						<h2>Contact Details</h2>
+
 						<img class="logo-large" src="<?php echo get_template_directory_uri(); ?>/assets/logos/large/symbol-color-large.png" alt="The Hummingbird Network logo">
 
-						<div class="division-one">
-							<p class="line-one">Contact Details</p>
-							<div class="address">
-								<p class="top-line">
-									<img class="location-marker" src="<?php echo get_template_directory_uri(); ?>/assets/elements/location-marker.svg" alt="location marker"><span>348 Tranquille Road</span>
-								</p>
-								<p>Kamloops BC</p>
-								<p>V2B 3G6</p>
-								<p>Canada</p>
-							</div>
-						</div><!-- division-one -->
+						<ul class="contact-details-one">
+							<li>
+								<span class="icon-spacer"><img src="<?php echo get_template_directory_uri(); ?>/assets/elements/location-marker.svg" alt="Location"></span>348 Tranquille Road
+							</li>
+							<li><span class="icon-spacer"></span>Kamloops BC</li>
+							<li><span class="icon-spacer"></span>V2B 3G6</li>
+							<li><span class="icon-spacer"></span>Canada</li>
+						</ul><!-- contact-details-one -->
 
-						<div class="division-two">
-
-							<div class="emailphone">
-								<p class="email">
-									<img class="email-icon" src="<?php echo get_template_directory_uri(); ?>/assets/elements/email-icon.svg" alt="email icon"><span>info@hummingbirdnetwork.ca</span>
-								</p>
-								<p class="phone">
-									<img class="phone-icon" src="<?php echo get_template_directory_uri(); ?>/assets/elements/phone-icon.svg" alt="phone icon"><span class="phone-number"> (250) 877.7082</span>
-								</p>
-							</div>
-
-							<div class="sm-icons">
-								<img src="<?php echo get_template_directory_uri(); ?>/assets/elements/facebook-icon.svg" alt="facebook icon">
-								<img src="<?php echo get_template_directory_uri(); ?>/assets/elements/instagram-icon.svg" alt="instagram icon">
-								<img src="<?php echo get_template_directory_uri(); ?>/assets/elements/twitter-icon.svg" alt="twitter icon">
-							</div>
+						<ul class="contact-details-two">
+							<li>
+								<span class="icon-spacer"><img src="<?php echo get_template_directory_uri(); ?>/assets/elements/email-icon.svg" alt="email icon"></span>info@hummingbirdnetwork.ca
+							</li>
+							<li>
+								<span class="icon-spacer"><img src="<?php echo get_template_directory_uri(); ?>/assets/elements/phone-icon.svg" alt="phone icon"></span>(250) 877.7082
+							</li>
+							<li>
+								<span class="icon-spacer"><img src="<?php echo get_template_directory_uri(); ?>/assets/elements/facebook-icon.svg" alt="facebook icon"></span>
+								<span class="icon-spacer"><img src="<?php echo get_template_directory_uri(); ?>/assets/elements/instagram-icon.svg" alt="instagram icon"></span>
+								<span class="icon-spacer"><img src="<?php echo get_template_directory_uri(); ?>/assets/elements/twitter-icon.svg" alt="twitter icon"></span>
+							</li>
 
 							<div class="contact-button">
 								<button class="contact-us button-link-solid">Contact Us</button>
 							</div>
-							
-						</div><!-- division-two -->
+
+						</ul><!-- contact-details-two -->
 
 					</div><!-- contact-details -->
-
-					<div class="copyrights">
-						<div class="blog">Blog</div>
-						<div class="faq">FAQ</div>
-						<div class="sitemap">Site Map</div>
-						<div class="pp">Privacy Policy</div>
-						<div class="copyright">c 2017 Hummingbird Network</div>
-						<div class="designed">Designed by Dream Team</div>
-						<div class="powered">Powered by Coffee</div>
-					</div>
 				
-					<!-- <a href="<?php echo esc_url( 'https://github.com/redacademy/hummingbird-fall-2017' ); ?>"><?php printf( esc_html( 'Proudly developed by %s' ), 'Nate, Travis, Hamdy' ); ?></a> -->
-
 				</div><!-- .site-info -->
+
+				<div class="copyrights-container">
+
+					<div class="copyrights-one width-content">
+						<span>Blog</span>
+						<span>FAQ</span>
+						<span>Site Map</span>
+						<span>Privacy Policy</span>
+						<span>c 2017 Hummingbird Network</span>
+					</div>
+
+					<div class="copyrights-two width-content">
+						<div>Designed by Dream Team</div>
+						<div>Powered by Coffee</div>
+					</div>
+
+				</div>
+
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 		
