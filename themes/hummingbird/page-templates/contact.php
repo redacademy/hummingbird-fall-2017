104d7c7f615bdc70bf62bfa0e9f0f22c0e4d580e
Late Worm <travis.rempel@gmail.com>
2017-12-13 14:28:28 -0800

    Apply basic styles to the Contact Us form.

--- themes/hummingbird/page-templates/contact.php
+++ themes/hummingbird/page-templates/contact.php
@@ -12,7 +12,7 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 
-			<section class="width-content">
+			<section class="contact-form width-content">
 				<?php while ( have_posts() ) : the_post(); ?>
 					<?php get_template_part( 'template-parts/content', 'page' ); ?>
 				<?php endwhile; // End of the loop. ?>
