8efab4241ba7181f950aa6b78f0a87b7c7a17b49
Late Worm <travis.rempel@gmail.com>
2017-12-13 10:08:36 -0800

    Improve typography

--- themes/hummingbird/header.php
+++ themes/hummingbird/header.php
@@ -12,7 +12,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-    <link href="https://fonts.googleapis.com/css?family=Merriweather:300,300i,400,400i,700,700i|Overpass:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:300,300i,400,400i,700,700i|Overpass:300,400,700" rel="stylesheet">
 		
 	<?php wp_head(); ?>
 	 
