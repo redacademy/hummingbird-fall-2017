bfffcb281e1e7735c655daddd5710c9e685957e9
hamdy <hamdyfarah13@gmail.com>
2017-12-13 22:59:51 -0800

    nav changes

--- themes/hummingbird/header.php
+++ themes/hummingbird/header.php
@@ -21,22 +21,20 @@
 	<body <?php body_class(); ?>>
 		<div id="page" class="hfeed site">
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
-			
+				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<div class="hb-logowhite"> 
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/logos/SVG/hummingbird-network-logo-white.svg" alt="hummingbird header logo">
+					</div>
 
-						<nav id="site-navigation" class="main-navigation" role="navigation">
-					
+					<div class="hb-logogreen"> 
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/logos/SVG/hummingbird-network-logo-color.svg" alt="hummingbird header white logo">
+					</div>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>			
 
-							<button class="menu-toggle">Menu</button>
-
-									<div class="hb-logo"> 
-							<img src="<?php echo get_template_directory_uri(); ?>/assets/logos/SVG/hummingbird-network-logo-color.svg" alt="hummingbird header logo">
-							</div>
-							<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>							
-
-			<header id="masthead" class="site-header" role="banner">
-				<div class="site-branding">
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				</div><!-- .site-branding -->
+					<header id="masthead" class="site-header" role="banner">
+					<div class="site-branding">
+						<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					</div><!-- .site-branding -->
 				</nav>
 				</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
