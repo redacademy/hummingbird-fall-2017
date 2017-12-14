11041789d73860561656c8fc645986ac61f729b5
Late Worm <travis.rempel@gmail.com>
2017-12-13 22:14:01 -0800

    Fix invalid syntax, restore DOM heirarchy.

--- themes/hummingbird/header.php
+++ themes/hummingbird/header.php
@@ -21,24 +21,18 @@
 	<body <?php body_class(); ?>>
 		<div id="page" class="hfeed site">
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
-			
-
-						<nav id="site-navigation" class="main-navigation" role="navigation">
-					
-
-							<button class="menu-toggle">Menu</button>
-
-									<div class="hb-logo">
-							<img src="<?php echo get_template_directory_uri(); ?>/assets/logos/SVG/hummingbird-network-logo-color.svg" alt="hummingbird header logo">
-							</div>
-							<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>							
 
 			<header id="masthead" class="site-header" role="banner">
-				<div class="site-branding">
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				</div><!-- .site-branding -->
-				</nav>
+
+				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<button class="menu-toggle">Menu</button>
+					<div class="site-branding hb-logo">
+						<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/logos/SVG/hummingbird-network-logo-color.svg" alt="hummingbird header logo">
+					</div><!-- .site-branding -->
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
 				</nav><!-- #site-navigation -->
+
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
