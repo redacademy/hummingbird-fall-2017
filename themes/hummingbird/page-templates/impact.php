c4b042ec8df73211dee95916124aa9656bc7142b
NCMoseley <ncmoseley@gmail.com>
2017-12-13 16:42:53 -0800

    Add styles to impact page

--- themes/hummingbird/page-templates/impact.php
+++ themes/hummingbird/page-templates/impact.php
@@ -15,7 +15,8 @@ get_header(); ?>
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 			<?php endwhile; // End of the loop. ?>
 
-			<section class="width-content">
+			<section class="width-content impact-header">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/elements/wolf.svg">
 				<h1><?php echo CFS()->get( 'impact_header_title' ); ?></h1>
 				<p><?php echo CFS()->get( 'impact_header_content' ); ?></p>
 			</section>
@@ -24,36 +25,50 @@ get_header(); ?>
 				<h1><?php echo CFS()->get( 'impact_header_case_title' ); ?></h1>
 			</section>
 
-			<section class="width-content">
-				<?php
-					$fields = CFS()->get( 'impact_header_case' );
-					foreach ( $fields as $field ) {
-						echo '<div>';
-							echo '<h3>';
-								echo $field['impact_header_case_title'];
-							echo '</h3>';
-							echo '<p>';
-								echo $field['impact_header_case_content'];
-							echo '<p>';
-						echo '</div>';
-					}
-				?>
+			<section class="impact-impact-case"> <!-- The three impact metrics -->
+				<?php $fields = CFS()->get( 'impact_impact_case' );
+
+				foreach ( $fields as $field ) : ?>
+
+					<div class="impact-impact-content">
+						<div class="flex-horizontal-left">
+							<div class="impact-impact-case-icon-container">
+								<img src=" <?php echo $field['impact_impact_case_icon'] ?> ">
+							</div>
+							<div>
+								<h3 class="impact-impact-case-number"><?php echo $field['impact_impact_case_number'] ?></h3>
+								<h3 class="impact-impact-case-metric"><?php echo $field['impact_impact_case_metric'] ?></h3>
+							</div>
+						</div>
+						<p><?php echo $field['impact_impact_case_content'] ?></p>
+					</div>
+					<div
+						class="width-full impact-impact-case-photo"
+						style="background-image: url(<?php echo $field['impact_impact_case_photo'] ?>);">
+					</div>
+
+				<?php endforeach; ?>
 			</section>
 
 			<section class="width-content">
-				<h1><?php echo CFS()->get( 'impact_where_title' ); ?></h1>
+				<h2><?php echo CFS()->get( 'impact_where_title' ); ?></h2>
 				<p><?php echo CFS()->get( 'impact_where_content' ); ?></p>
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/elements/maps-home-page.png">
 			</section>
 
 			<section class="width-content">
 				<?php
 					$fields = CFS()->get( 'impact_call_case' );
 					foreach ( $fields as $field ) {
-						echo '<h1>';
+						echo '<h2>';
 						echo $field['impact_call_case_title'];
-						echo '</h1>';
+						echo '</h2>';
 					}
 				?>
+			</section>
+
+			<section class="width-content flex-horizontal-center get-involved">
+				<div><a href="<?php echo esc_url( home_url( 'take-action/' ) ); ?>" class="contact-button button-link button-link-solid">Get Involved</a></div>
 			</section>
 
 
