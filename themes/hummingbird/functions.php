0a7883bac8e9998bcb331f603d48a3f397f9ce6c
hamdy <hamdyfarah13@gmail.com>
2017-12-15 10:37:13 -0800

    Font adjustments

--- themes/hummingbird/functions.php
+++ themes/hummingbird/functions.php
@@ -176,3 +176,9 @@ function my_login_logo_url_title() {
     return 'Your Site Name and Info';
 }
 add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+
+function myprefix_enqueue_google_fonts() { 
+	wp_enqueue_style( 'merriweather', 'https://fonts.googleapis.com/css?family=Merriweather' ); 
+	wp_enqueue_style( 'overpass', 'https://fonts.googleapis.com/css?family=Overpass' ); 
+}
+add_action( 'wp_enqueue_scripts', 'myprefix_enqueue_google_fonts' ); 
