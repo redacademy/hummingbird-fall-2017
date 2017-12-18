46e32c27c027075d3577944283de66425d4fa006
hamdy <hamdyfarah13@gmail.com>
2017-12-18 11:00:26 -0800

    fix merge conflict?

--- themes/hummingbird/functions.php
+++ themes/hummingbird/functions.php
@@ -118,7 +118,7 @@ function cfp($atts, $content = null) {
 			$p = trim($p);
 
 add_shortcode('cfp', 'cfp');
-
+	}
 // wp_enqueue_script( 'wpb_togglemenu', get_template_directory_uri() . '/js/navigation.js', array('jquery'), '20160909', true );
 
 function hn_login_logo() { ?>
