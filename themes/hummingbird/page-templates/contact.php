132d399481c2d9e3853742f906209588d79f3561
NCMoseley <ncmoseley@gmail.com>
2017-12-18 17:23:43 -0800

    Add contact information

--- themes/hummingbird/page-templates/contact.php
+++ themes/hummingbird/page-templates/contact.php
@@ -16,6 +16,39 @@ get_header(); ?>
 				<h1>Contact Us</h1>
 			</section>
 
+						<div class="contact-details-container width-content">
+								<h2>Contact Details</h2>
+							<div class="contact-details-wrapper"> <!-- container div for flexbox purposes -->
+								
+								<ul class="contact-details-one">
+									<li>
+										<span class="icon-spacer"><img src="<?php echo get_template_directory_uri(); ?>/assets/elements/location-marker-black.svg" alt="Location"></span><?php echo CFS()->get( 'front_page_address_1', 2 ); ?>
+									</li>
+									<li><span class="icon-spacer"></span><?php echo CFS()->get( 'front_page_address_2', 2 ); ?></li>
+									<li><span class="icon-spacer"></span><?php echo CFS()->get( 'front_page_address_3', 2 ); ?></li>
+									<li><span class="icon-spacer"></span><?php echo CFS()->get( 'front_page_address_4', 2 ); ?></li>
+								</ul><!-- contact-details-one -->
+							</div>
+
+							<div class="contact-details-wrapper-two"> <!-- container div for flexbox purposes -->
+								<ul class="contact-details-two">
+									<li>
+										<span class="icon-spacer"><img src="<?php echo get_template_directory_uri(); ?>/assets/elements/email-icon-black.svg" alt="email icon"></span><?php echo CFS()->get( 'front_page_email_address', 2 ); ?>
+									</li>
+									<li>
+										<span class="icon-spacer"><img src="<?php echo get_template_directory_uri(); ?>/assets/elements/phone-icon-black.svg" alt="phone icon"></span><?php echo CFS()->get( 'front_page_phone_number', 2 ); ?>
+									</li>
+								</ul><!-- contact-details-two -->
+							</div>
+
+							<div class="contact-details-three">
+										<span class="icon-spacer social-media-icon"><img src="<?php echo get_template_directory_uri(); ?>/assets/elements/facebook-icon-orange.svg" alt="facebook icon"></span>
+										<span class="icon-spacer social-media-icon"><img src="<?php echo get_template_directory_uri(); ?>/assets/elements/instagram-icon-orange.svg" alt="instagram icon"></span>
+										<span class="icon-spacer social-media-icon"><img src="<?php echo get_template_directory_uri(); ?>/assets/elements/twitter-icon-orange.svg" alt="twitter icon"></span>
+							</div><!-- contact-details-three -->
+
+						</div><!-- contact-details -->
+
 			<section class="contact-form width-content">
 				<?php while ( have_posts() ) : the_post(); ?>
 					<?php get_template_part( 'template-parts/content', 'page' ); ?>
